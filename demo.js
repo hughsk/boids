c7f17a3eee27c72dd5a4e7aa58b201deed0af44a
Hugh Kennedy <hughskennedy@gmail.com>
2013-05-02 11:40:06 +1000

    fix up demo

--- demo.js
+++ demo.js
@@ -14,19 +14,6 @@ var canvas = document.createElement('canvas')
       boids: 200
     , speedLimit: 2
     , attractors: attractors
-  }).on('tick', function(boids) {
-    var halfHeight = canvas.height/2
-      , halfWidth = canvas.width/2
-
-    ctx.fillStyle = '#000'
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
-
-    ctx.fillStyle = '#fff'
-    for (var i = 0, l = boids.length; i < l; i += 1) {
-      ctx.fillRect(boids[i].pos[0] + halfWidth, boids[i].pos[1] + halfHeight, 5, 5)
-    }
-  }).once('tick', function(boids) {
-    console.log(boids)
   })
 
 document.body.onmousemove = function(e) {
@@ -35,8 +22,6 @@ document.body.onmousemove = function(e) {
 
   attractors[0][0] = e.x - halfWidth
   attractors[0][1] = e.y - halfHeight
-  attractors[1][0] = e.x - halfWidth
-  attractors[1][1] = e.y - halfHeight
 }
 
 canvas.width = window.innerWidth
@@ -46,4 +31,22 @@ document.body.style.margin = '0'
 document.body.style.padding = '0'
 document.body.appendChild(canvas)
 
-raf(window).on('data', boids.tick.bind(boids))
+var tock = 0
+raf(window).on('data', function() {
+  var boidData = boids.boids
+
+  tock += 1
+  tock = tock % 3
+  boids.tick()
+
+  var halfHeight = canvas.height/2
+    , halfWidth = canvas.width/2
+
+  ctx.fillStyle = '#000'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+  ctx.fillStyle = '#fff'
+  for (var i = 0, l = boidData.length; i < l; i += 1) {
+    ctx.fillRect(boidData[i][0] + halfWidth, boidData[i][1] + halfHeight, 5, 5)
+  }
+})
